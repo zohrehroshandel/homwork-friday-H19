4f3ed759683a1d9cc57c2c42682059b96c308acd
Soroush4208 <soroush.r80@gmail.com>
2025-12-26 16:34:18 +0330

    feat:create form

--- src/components/home/form/form.tsx
+++ src/components/home/form/form.tsx
@@ -1,17 +1,43 @@
-import type { FormEventHandler } from "react";
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
-type LoginData = {};
+// type LoginData = {};
 
 export default function Form() {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const data = Object.fromEntries(formData.entries()) as LoginData;
+  // const handleSubmitOne: FormEventHandler<HTMLFormElement> = e => {
+  //   e.preventDefault();
+  //   const formData = new FormData(e.currentTarget);
+  //   const dataSubmitted = Object.fromEntries(formData.entries());
+  // };
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+    reset();
   };
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="your Compony Name"
+        {...register('ComponyName', {
+          required: 'Compony Name is required',
+          minLength: {
+            value: 4,
+            message: 'Compony Name must be at least 2 characters',
+          },
+        })}
+      />
+      {errors.ComponyName && (
+        <p className="text-red-500 font-bold text-md">
+          {errors.ComponyName?.message}
+        </p>
+      )}
+      <button type="submit">Submit</button>
     </form>
   );
 }
