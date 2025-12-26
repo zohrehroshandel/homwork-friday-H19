4882ef9e64842b5f11cb4a527ba8409ebc1ac5df
Simin Hedayati <hedayati@gamelectronics.com>
2025-12-26 16:20:38 +0330

    added form handler

--- src/components/home/form/form.tsx
+++ src/components/home/form/form.tsx
@@ -1,10 +1,17 @@
+import type { FormEventHandler } from "react";
+import { useForm } from "react-hook-form";
 
+type LoginData = {};
 
 export default function Form() {
-
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries()) as LoginData;
+  };
   return (
-    <div>
-    <input type="text" />
-    </div>
-  )
+    <form>
+      <input type="text" />
+    </form>
+  );
 }
